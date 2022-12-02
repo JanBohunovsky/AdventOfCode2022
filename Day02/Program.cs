c12dd6986d336279ed2d5b1dc310d16f68c01ca9
Jan Bohunovský <janbohunovsky@protonmail.com>
2022-12-02 14:44:13 +0100

    Day 2

---
+++ Day02/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+
+var input = File.ReadAllLines("Data/input.txt")
+	.Select(l => new
+	{
+		Opponent = l[0] switch
+		{
+			'A' => Shape.Rock,
+			'B' => Shape.Paper,
+			'C' => Shape.Scissors,
+			_ => throw new UnreachableException(),
+		},
+		You = l[2],
+	})
+	.ToArray();
+
+
+// Part 1
+var totalScoreForAssumedStrategy = input
+	.Select(i => new
+	{
+		i.Opponent,
+		You = i.You switch
+		{
+			'X' => Shape.Rock,
+			'Y' => Shape.Paper,
+			'Z' => Shape.Scissors,
+			_ => throw new UnreachableException(),
+		},
+	})
+	.Sum(i => CalculateScore(i.Opponent, i.You));
+
+Console.WriteLine($"Part 1: {totalScoreForAssumedStrategy}");
+
+
+// Part 2
+var totalScoreForActualStrategy = input
+	.Select(i => new
+	{
+		i.Opponent,
+		You = i.You switch
+		{
+			'X' => ShapeHelper.GetLosingShape(i.Opponent),
+			'Y' => i.Opponent,
+			'Z' => ShapeHelper.GetWinningShape(i.Opponent),
+			_ => throw new UnreachableException(),
+		},
+	})
+	.Sum(i => CalculateScore(i.Opponent, i.You));
+
+Console.WriteLine($"Part 2: {totalScoreForActualStrategy}");
+
+
+int CalculateScore(Shape opponent, Shape you)
+{
+	var shapeScore = (int)you;
+	int outcomeScore;
+
+	if (opponent == you)
+	{
+		outcomeScore = 3;
+	}
+	else if (opponent.IsBeatenBy(you))
+	{
+		outcomeScore = 6;
+	}
+	else
+	{
+		outcomeScore = 0;
+	}
+
+	return shapeScore + outcomeScore;
+}
---
+++ Day02/Shape.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+internal enum Shape
+{
+	Rock = 1,
+	Paper = 2,
+	Scissors = 3,
+}
+
+internal static class ShapeHelper
+{
+	internal static bool IsBeatenBy(this Shape shape, Shape other)
+	{
+		return GetWinningShape(shape) == other;
+	}
+
+	/// <summary>
+	/// Returns a <see cref="Shape"/> that beats this <paramref name="shape"/>.
+	/// </summary>
+	internal static Shape GetWinningShape(Shape shape)
+	{
+		return shape switch
+		{
+			Shape.Rock => Shape.Paper,
+			Shape.Paper => Shape.Scissors,
+			Shape.Scissors => Shape.Rock,
+			_ => throw new UnreachableException(),
+		};
+	}
+
+	/// <summary>
+	/// Returns a <see cref="Shape"/> that is beaten by this <paramref name="shape"/>.
+	/// </summary>
+	internal static Shape GetLosingShape(Shape shape)
+	{
+		return shape switch
+		{
+			Shape.Rock => Shape.Scissors,
+			Shape.Paper => Shape.Rock,
+			Shape.Scissors => Shape.Paper,
+			_ => throw new UnreachableException(),
+		};
+	}
+}
