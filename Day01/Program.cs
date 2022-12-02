89703e7cf1ed01a166419d6471e107bd48f3de97
Jan Bohunovský <janbohunovsky@protonmail.com>
2022-12-02 13:52:59 +0100

    Day 1

---
+++ Day01/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = File.ReadAllText("Data/input.txt")
+	.Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+	.Select(g => g
+		.Split('\n', StringSplitOptions.RemoveEmptyEntries)
+		.Select(int.Parse)
+		.ToArray())
+	.ToArray();
+
+// Part 1
+var mostCalories = input.Select(g => g.Sum())
+	.OrderDescending()
+	.First();
+
+Console.WriteLine($"Part 1: {mostCalories}");
+
+// Part 2
+var topThreeMostCalories = input
+	.Select(g => g.Sum())
+	.OrderDescending()
+	.Take(3)
+	.Sum();
+
+Console.WriteLine($"Part 2: {topThreeMostCalories}");
