a2428e15fa6090a69f00ad6684926ee97f630104
Jan Bohunovsky <jbohunovsky@idc.com>
2022-12-06 08:52:16 +0100

    Day 6

---
+++ Day06/Program.cs
@@ -0,0 +1,29 @@
+﻿const int startOfPacketSize = 4;
+const int startOfMessageSize = 14;
+var input = File.ReadAllText("Data/input.txt");
+
+var startOfPacket = FindMarker(input, startOfPacketSize);
+Console.WriteLine($"Part 1: {startOfPacket}");
+
+var startOfMessage = FindMarker(input, startOfMessageSize);
+Console.WriteLine($"Part 2: {startOfMessage}");
+
+
+int FindMarker(string stream, int markerSize)
+{
+	for (int i = 0; i < stream.Length - markerSize; i++)
+	{
+		var uniqueCharCount = stream
+			.Skip(i)
+			.Take(markerSize)
+			.Distinct()
+			.Count();
+
+		if (uniqueCharCount == markerSize)
+		{
+			return i + markerSize;
+		}
+	}
+
+	throw new Exception($"Stream does not contain marker that has {markerSize} distinct characters");
+}
